794bb8ad67ed19a8251e8203aebc8206bad9a334
roshanmurkar <85471797+roshanmurkar@users.noreply.github.com>
2021-07-06 19:33:23 +0530

    conflict created

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.printLn("File is updated-conflict is created");
+		System.out.printLn("File is updated");
 	}
 
 }
